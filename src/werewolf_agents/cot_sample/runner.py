eb49efc702cde338541f97db17b96b1286a32e5b
clayschubiner <cschubiner@gmail.com>
2024-11-09 19:10:47 -0800

    more

--- src/werewolf_agents/cot_sample/runner.py
+++ src/werewolf_agents/cot_sample/runner.py
@@ -13,7 +13,7 @@ Path("transcripts").mkdir(parents=True, exist_ok=True)
 
 runner = WerewolfCampaignActivityRunner()
 agent_config = PlayerAgentConfig(
-    player_name="Marcus", # select a name for your agent
+    player_name="Katie", # select a name for your agent
     agent_wheel_path="dist/chagent-0.1.0-py3-none-any.whl",
     module_path="agent/cot_agent.py",
     agent_class_name="CoTAgent",
